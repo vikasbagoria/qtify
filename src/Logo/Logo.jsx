b5bb7aa3a0ae266483d297e97ef744cfaf58e217
vikasbagoria <vikasbagoria@gmail.com>
2025-03-03 16:47:53 +0530

    Initial commit

---
+++ src/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoImage from "../assets/logo.png";
+
+
+export default function Logo() {
+  return <img src={LogoImage} alt="logo" width={67} />;
+}
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import Search from "../Search/Search";
+import styles from "./Navbar.module.css";
+
+function Navbar({ searchData }) {
+  return (
+    <nav className={styles.navbar}>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <div className={styles["search-container"]}>
+        <Search placeholder="Search a song of your choice" searchData={searchData} />
+      </div>
+      <Button text="Give Feedback" className={styles["feedback-button"]} />
+    </nav>
+  );
+}
+
+export default Navbar;
